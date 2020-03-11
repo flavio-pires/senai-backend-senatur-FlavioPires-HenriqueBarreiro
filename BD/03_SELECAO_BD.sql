1a0a41c4a2550a1e2a2d239fc0ceb4fe4df8cd97
Henrique Barreiro Santana <47651807863@info.local>
2020-03-11 11:25:10 -0300

    Projeto Finalizado

--- BD/03_SELECAO_BD.sql
+++ BD/03_SELECAO_BD.sql
@@ -11,3 +11,20 @@ SELECT Email, Senha, TiposUsuario.Titulo FROM Usuarios INNER JOIN TiposUsuario O
 
 --Listar os pacotes e mostrar o nome da cidade.
 SELECT NomePacote, Descricao, DataIda, DataVolta, Valor, Ativo, Cidades.NomeCidade FROM Pacotes INNER JOIN Cidades ON Pacotes.IdCidade = Cidades.IdCidade;
+
+
+--Listar somente os Pacotes Ativos.
+SELECT NomePacote, Descricao, DataIda, DataVolta, Valor, Ativo, IdCidade FROM Pacotes WHERE Ativo = 1; 
+
+--Listar somente os Pacotes Inativos.
+SELECT NomePacote, Descricao, DataIda, DataVolta, Valor, Ativo, IdCidade FROM Pacotes WHERE Ativo = 0;
+
+--Listar somente os Pacotes para uma determinada cidade.
+SELECT NomePacote, Descricao, DataIda, DataVolta, Valor, Ativo, Cidades.NomeCidade FROM Pacotes INNER JOIN Cidades ON Pacotes.IdCidade = Cidades.IdCidade
+WHERE Cidades.IdCidade = 1;
+
+--Listar os Pacotes com ordenação por preço, do mais barato para o mais caro.
+SELECT NomePacote, Descricao, DataIda, DataVolta, Valor, Ativo, IdCidade FROM Pacotes ORDER BY Valor DESC;
+
+--Listar os Pacotes com ordenação por preço, do mais caro para o mais barato.
+SELECT NomePacote, Descricao, DataIda, DataVolta, Valor, Ativo, IdCidade FROM Pacotes ORDER BY Valor ASC;
